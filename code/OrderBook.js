56cbe3e1a40ffbbf45ce744e4e83424fab46b8df
Nick Groos <ntgroos@gmail.com>
2021-04-13 14:51:36 -0500

    poc improvements

--- code/OrderBook.js
+++ code/OrderBook.js
@@ -1,6 +1,63 @@
+const fetch = require('node-fetch');
+
 class OrderBook {
     constructor() {
         console.log('in order book constructor');
+        this.bookInitialized = false;
+        this.messages = {};
+    }
+    
+    getCBSnapshot() {
+         // use REST api to get current book snapshot
+         // GET /products/BTC-USD/book
+
+
+         fetch('https://api-public.sandbox.pro.coinbase.com/products/BTC-USD/book?level=3')
+         .then(res => res.json())
+         .then((json) => {
+             // Response looks like:
+             /*
+                { 
+                    bids: [
+                        [ '62883.81', '4771', '2b6e645c-29c8-4333-81b0-042932296317' ],
+                        [ '62883.8', '6020', '1c75604c-274d-4474-9560-1c844393d84e' ],
+                        [ '62883.79', '6989', '467a8da8-15cd-4836-bece-1cbd9983d89a' ]
+                ], 
+                asks: [
+                        [ '62883.85', '4771', '8fc58e9d-d3ed-49da-b686-77cca3bb04cd' ],
+                        [ '62883.86', '6020', '0f721713-e12b-43bb-a4b1-5e44a0135055' ],
+                        [ '62883.87', '6989', 'e4116e21-74b8-4381-986b-d699425e8441' ]
+                ] 
+            }
+             */
+             console.log(json);
+         });
+
+         // this will need to be async
+        //return new Promise();
+    }
+
+    processMessages() {
+
+    }
+
+    queueMessage(message) {
+        if (!this.bookInitialized) {
+            // use REST api to get current book snapshot
+            getCBSnapshot();
+            //processMessages();
+        };
+
+        // open - add it to this book
+
+        // done - remove it from this book
+
+        // match - a trade or partial trade occurred 
+            // - remove it from the book, or update the size remaining if it's partial
+            // this is a Tick (Market Event) - when this happens, we want to print the current best bid/ask in the book
+
+        // change - an existing order needs to be changed
+
     }
 
     printTickInfo() {
--- index.js
+++ index.js
@@ -1,56 +1,42 @@
 const { OrderBook } = require('./code/OrderBook');
 const WebSocket = require('ws');
 
-const cbWebsocket = 'wss://ws-feed.pro.coinbase.com';
+const cbWebsocket = 'wss://ws-feed-public.sandbox.pro.coinbase.com';
 
 // {"type":"subscriptions","channels":[{"name":"level2","product_ids":["BTC-USD"]},{"name":"heartbeat","product_ids":["BTC-USD"]},{"name":"ticker","product_ids":["BTC-USD"]}]}
 // {"type":"snapshot","product_id":"BTC-USD","asks":[["63297.76","2.94530258"],["63297.77","0.00650000"].....
 
 const run = () => {
 
-    //const book = new OrderBook();
+    const book = new OrderBook();
 
-    // connect to wss://ws-feed.pro.coinbase.com via websocket
+    book.getCBSnapshot();
+
+    // connect to wss://ws-feed-public.sandbox.pro.coinbase.com via websocket
     // subscribe to BTC-USD market data
     const wsClient = new WebSocket(cbWebsocket)
 
     wsClient.on('message', (msg) => {
+        const parsedMessage = JSON.parse(msg);
+        console.log(`message type: ${parsedMessage.type}`);
+        console.log(parsedMessage);
 
-        const parsed = JSON.parse(msg);
-        console.log(`message type: ${parsed.type}`);
-
-        if (parsed.type === 'subscriptions') {
-            console.log('got a subscription');
-        }
-
-        if (parsed.type === 'l2update') {
-            console.log('got an l2update');
-            //
-        }
-
-        if (parsed.type === 'ticker') {
-            // get the proper ask/bid levels for this tick and print them
-
-            // reset book
-            console.log('got a tick');
-        }
-
-        console.log(msg);
+        //book.queueMessage(parsedMessage);
     });
+
+    // wsClient.on('open', () => {
+    //     const subscribeRequest = {
+    //         type: 'subscribe',
+    //         product_ids: ['BTC-USD'],
+    //         channels: ['full']
+    //     };
+
+    //     console.log('sending subscribe request');
+    //     wsClient.send(JSON.stringify(subscribeRequest));
+    // });
 
     wsClient.on('close', () => {
         console.log('connection closed');
-    })
-
-    wsClient.on('open', () => {
-        const subscribeRequest = {
-            type: 'subscribe',
-            product_ids: ['BTC-USD'],
-            channels: ['full']
-        };
-
-        console.log('sending subscribe request');
-        wsClient.send(JSON.stringify(subscribeRequest));
     });
 };
 
