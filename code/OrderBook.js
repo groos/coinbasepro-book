06e1ba52dab9cbaf2335eca30715be0b54dbf4a7
Nick Groos <ntgroos@gmail.com>
2021-04-13 14:03:45 -0500

    poc setup

---
+++ code/OrderBook.js
@@ -0,0 +1,27 @@
+class OrderBook {
+    constructor() {
+        console.log('in order book constructor');
+    }
+
+    printTickInfo() {
+        // get the "best" bids and ask fors the current tick and print them like
+
+        /*
+            1.50000 @ 60858.43
+            0.50000 @ 60858.27
+            0.28740 @ 60857.64
+            0.48960 @ 60852.87
+            0.40010 @ 60851.26
+            ---------------------
+            0.07000 @ 60839.02
+            0.45060 @ 60832.34
+            0.29780 @ 60830.87
+            1.50000 @ 60820.31
+            0.50000 @ 60818.47
+        */
+    }
+}
+
+module.exports = {
+    OrderBook
+}
